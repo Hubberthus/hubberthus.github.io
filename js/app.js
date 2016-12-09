f38e3bb678d2f77ce814e643b461674e6a121ea1
Unknown <hubberthus@gmail.com>
2016-12-09 22:55:31 +0100

    Some initial test code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,14 @@
 var app = angular.module("mcuSetupApp", []); 
 app.controller("mcuSetupController", function($scope) {
 	
-    $scope.products = ["Milk", "Bread", "Cheese"];
-    
+	$scope.cores = [];
+	
+	$.ajax({
+	  url: "/cores/",
+	  success: function(data){
+	     $(data).find("a:contains(.json)").each(function(){
+	        $scope.cores = $(this).attr("href");
+	     });
+	  }
+	});
 });
