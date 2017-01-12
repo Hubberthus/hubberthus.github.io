cb3ac034ec67810551455b32d876ea9ae85809b1
Norbert Fekete <norbert.fekete@ericsson.com>
2017-01-12 16:10:44 +0100

    Pin mapping for output at selection

--- app/controller.js
+++ app/controller.js
@@ -13,5 +13,18 @@ define([
 		
 		$scope.cores = cores.loadCores();
 		$scope.modules = modules.loadModules();
+
+		$scope.pin_map = new Array($scope.modules[0].pins.length);
+		
+		var i = 1;
+		
+		$scope.modules[0].pins.forEach(function( pin ) {
+			
+			if (pin.number) {
+				$scope.pin_map[pin.number] = i;
+			}
+		
+			i++;
+		});
 	});
 });
