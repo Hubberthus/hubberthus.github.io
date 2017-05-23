6e9f4516f0353bfb82af22805f4e8bed74907ae5
Norbert Fekete <norbert.fekete@ericsson.com>
2017-05-23 11:33:07 +0200

    Adding/removing pin arrays, and moving with mouse

--- js/editor.js
+++ js/editor.js
@@ -26,7 +26,41 @@ define([
 	// Editor controller function for the AngularJS application	
 	app.controller('mcuSetupEditorController',function($scope) {
 		
+		$scope.mouseX = -1;
+		$scope.mouseY = -1;
+		$scope.mouseOffsetX = -1;
+		$scope.mouseOffsetY = -1;
+		
 		$scope.active_module = $scope.modules[0];
+		
+		if ($scope.active_module.arrays && $scope.active_module.arrays.length > 0) {
+			$scope.active_array = $scope.active_module.arrays[0];
+		}
+		
+		$scope.newArray = function() {
+			var array = {
+				position: {
+					x: 0,
+					y: 0
+					},
+				pitch: 5,
+				orientation: "vertical",
+				side: "left",
+				pins: [Object.keys($scope.active_module.pins)[0]]
+				};
+			
+			$scope.active_module.arrays.push(array);
+			$scope.active_array = array;
+			$scope.active_pin = null;
+		}
+		
+		$scope.removeArray = function() {
+			
+			var i = $scope.active_module.arrays.indexOf($scope.active_array);
+			$scope.active_module.arrays.splice(i, 1);
+			$scope.active_array = null;
+			$scope.active_pin = null;
+		}
 		
 		$scope.setActiveArrayAndPin = function( array, pin ) {
 			
@@ -63,13 +97,25 @@ define([
 			$scope.active_array.side = value;
 		}
 		
-		$scope.mouseX = -1;
-		$scope.mouseY = -1;
+		$scope.mouseDown = function( event ) {
+			
+			if (event.buttons == 1) {
+				$scope.mouseOffsetX = $scope.mouseX;
+				$scope.mouseOffsetY = $scope.mouseY;
+			}
+		}
 		
-		$scope.mouseMove = function( event ) {
+		$scope.mouseMove = function( event, array ) {
 			
 			$scope.mouseX = event.offsetX / document.getElementById('module-image').offsetWidth * 100 - 5;
 			$scope.mouseY = event.offsetY / document.getElementById('module-image').offsetHeight * 100 - 5;
+			
+			if (($scope.active_array == array) && (event.buttons == 1)) {
+				if ((Math.abs($scope.mouseX - $scope.mouseOffsetX) > 1) || (Math.abs($scope.mouseY - $scope.mouseOffsetY) > 1)) {
+					$scope.moveArrayPositionX($scope.mouseX - $scope.mouseOffsetX);
+					$scope.moveArrayPositionY($scope.mouseY - $scope.mouseOffsetY);
+				}
+			}
 		}
 	})
 	.directive('editorPins', function() {
